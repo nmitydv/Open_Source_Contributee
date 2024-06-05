61de03fa0c0ce697926943c2686813848119d2b7
Namit Yadav <yadavnamit605@gmail.com>
2024-06-05 14:16:01 +0900

    json

--- Json_Ajax/exe1.js
+++ Json_Ajax/exe1.js
@@ -1,11 +1,26 @@
 const namitInfo = {
-    fathername:"Khushal yadav",
-    caste:"Yadav",
-    age:"19 years"
+    fathername: "Khushal yadav",
+    caste: "Yadav",
+    age: "19 years"
 }
 console.log("This is object:")
 console.log(namitInfo.age);
 
-const namitMarks= ['a', 2, 3, 4, 5, 6, 7, 8, 9];
+const namitMarks = ['a', 2, 3, 4, 5, 6, 7, 8, 9];
 console.log("This is Array:");
 console.log(namitMarks[1]);
+
+
+const myJson = [
+    {
+        "name": "John",
+        "age": "22"
+    },
+    {
+        "name": "Deo",
+        "age": "21"
+    }
+]
+console.log("This is Json")
+var name1 = myJson[1].name;
+console.log(name1);
