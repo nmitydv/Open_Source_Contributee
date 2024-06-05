3d7595aa584c2a0812167ddea42eaf59c9f7afa1
Namit Yadav <yadavnamit605@gmail.com>
2024-06-05 14:08:15 +0900

    i learnt object and arrays

--- Json_Ajax/exe1.js
+++ Json_Ajax/exe1.js
@@ -1,6 +1,11 @@
-const a = {
-    "name": "namit",
-    "age": 13
+const namitInfo = {
+    fathername:"Khushal yadav",
+    caste:"Yadav",
+    age:"19 years"
 }
+console.log("This is object:")
+console.log(namitInfo.age);
 
-console.log(a.name);
+const namitMarks= ['a', 2, 3, 4, 5, 6, 7, 8, 9];
+console.log("This is Array:");
+console.log(namitMarks[1]);
